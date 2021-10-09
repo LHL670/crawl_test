c9d7a0267f2460c7afe55e7bb57e42bca0e7b70a
LHL670 <sally12066@gmail.com>
2021-10-09 16:28:10 +0800

    add label crawl

--- CGUScholarLabel.py
+++ CGUScholarLabel.py
@@ -10,7 +10,7 @@ def get_LabelIDList(label):
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
     }
     r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text)
+    soup = BeautifulSoup(r.text, "html.parser")
 
     searchPage = True
     tempList = []
