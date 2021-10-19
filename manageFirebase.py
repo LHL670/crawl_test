1446b0a4a5d7a4a157f40cba24094e8443b73c1a
LHL670 <sally12066@gmail.com>
2021-10-19 11:43:16 +0800

    fix problem

--- manageFirebase.py
+++ manageFirebase.py
@@ -14,15 +14,19 @@ def update_personaldata(personalData):
 
 def update_labelinfo(item, label):
     items = jsonTransfer.jsontransform(item)
-    print(label)
+    print(items)
     ref = db.collection(u'Label-Domain').document(label)
-    ref.set(item)
+    ref.set(items)
 
 
 def update_labeldomain(label):
     for i in label:
         ref = db.collection(u'Label-Domain').document(i)
-        ref.set({u'updateTime': None})
+        doc = ref.get()
+        if doc.exists:
+            print('exist')
+        else:
+            ref.set({u'updateTime': None})
 
 
 def get_lastupdatelabel():
